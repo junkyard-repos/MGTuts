6195cb8ff6e46d0d3505e3bcbc0162663e68093b
David Long <davdaarn@users.noreply.github.com>
2020-08-31 16:55:33 -0600

    Init new project

---
+++ RPGMadeEasy/Game.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace RPGMadeEasy
+{
+    public class Game : Microsoft.Xna.Framework.Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        public Game()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            graphics.PreferredBackBufferWidth = (int)ScreenManager.Instance.Dimensions.X;
+            graphics.PreferredBackBufferHeight = (int)ScreenManager.Instance.Dimensions.Y;
+            graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            ScreenManager.Instance.LoadContent(Content);
+        }
+
+        protected override void UnloadContent()
+        {
+            ScreenManager.Instance.UnloadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            ScreenManager.Instance.Update();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            ScreenManager.Instance.Draw(spriteBatch);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ RPGMadeEasy/ScreenManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RPGMadeEasy
+{
+    public class ScreenManager
+    {
+        private static ScreenManager instance;
+        public Vector2 Dimensions { get; private set; }
+
+        public static ScreenManager Instance
+        {
+            get 
+            {
+                if (instance == null)
+                {
+                    instance = new ScreenManager();
+                }
+
+                return instance;
+            }
+        }
+
+        public ScreenManager()
+        {
+            Dimensions = new Vector2(640, 480);
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+
+        }
+
+        public void UnloadContent() { }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch) { }
+
+        
+    }
+}
