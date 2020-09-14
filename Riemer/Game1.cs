3acdd003ae751509754ffec11b6868dd420c2623
David Long <davdaarn@users.noreply.github.com>
2020-09-14 10:25:25 -0600

    Specifying the position of an image on the screen

--- Riemer/Game1.cs
+++ Riemer/Game1.cs
@@ -1,19 +1,53 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Runtime.Serialization;
 
 namespace Riemer
 {
+    public struct PlayerData
+    {
+        public Color Color;
+        public Vector2 Position;
+
+        public float Angle;
+        public float Power;
+        public bool IsAlive;
+    }
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        #region properties
+
         private GraphicsDevice _device;
+        private SpriteBatch _spriteBatch;
+        private GraphicsDeviceManager _graphics;
+
         private Texture2D _backgroundTexture;
         private Texture2D _foregroundTexture;
+        private Texture2D _carriageTexture;
+        private Texture2D _cannonTexture;
 
         private int _screenWidth;
         private int _screenHeight;
+
+        private PlayerData[] _players;
+        private int _numberOfPlayers = 4;
+
+        private Color[] _playerColors = new Color[10]
+        {
+            Color.Red,
+            Color.Green,
+            Color.Blue,
+            Color.Purple,
+            Color.Orange,
+            Color.Indigo,
+            Color.Yellow,
+            Color.SaddleBrown,
+            Color.Tomato,
+            Color.Turquoise
+        };
+
+        #endregion
 
         public Game1()
         {
@@ -33,6 +67,23 @@ namespace Riemer
             base.Initialize();
         }
 
+        private void SetUpPlayers()
+        {
+            _players = new PlayerData[_numberOfPlayers];
+            for (int i = 0; i < _numberOfPlayers; i++)
+            {
+                _players[i].IsAlive = true;
+                _players[i].Color = _playerColors[i];
+                _players[i].Angle = MathHelper.ToRadians(90);
+                _players[i].Power = 100;
+            }
+
+            _players[0].Position = new Vector2(100, 193);
+            _players[1].Position = new Vector2(200, 212);
+            _players[2].Position = new Vector2(300, 361);
+            _players[3].Position = new Vector2(400, 164);
+        }
+
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
@@ -44,6 +95,10 @@ namespace Riemer
 
             _backgroundTexture = Content.Load<Texture2D>("Riemer/background");
             _foregroundTexture = Content.Load<Texture2D>("Riemer/foreground");
+            _carriageTexture = Content.Load<Texture2D>("Riemer/carriage");
+            _cannonTexture = Content.Load<Texture2D>("Riemer/cannon");
+
+            SetUpPlayers();
         }
 
         protected override void Update(GameTime gameTime)
@@ -63,6 +118,7 @@ namespace Riemer
             _spriteBatch.Begin();
 
             DrawScenery();
+            DrawPlayers();
 
             _spriteBatch.End();
 
@@ -74,6 +130,17 @@ namespace Riemer
             Rectangle screenRectangle = new Rectangle(0, 0, _screenWidth, _screenHeight);
             _spriteBatch.Draw(_backgroundTexture, screenRectangle, Color.White);
             _spriteBatch.Draw(_foregroundTexture, screenRectangle, Color.White);
+        }
+
+        private void DrawPlayers()
+        {
+            for (int i = 0; i < _players.Length; i++)
+            {
+                if (_players[i].IsAlive)
+                {
+                    _spriteBatch.Draw(_carriageTexture, _players[i].Position, _players[i].Color);
+                }
+            }
         }
     }
 }
