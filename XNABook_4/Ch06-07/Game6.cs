24727094765c6f342ed1ba985623b9f008855b34
David Long <davdaarn@users.noreply.github.com>
2020-09-22 15:51:57 -0600

    Collision detection

--- XNABook_4/Ch06-07/Game6.cs
+++ XNABook_4/Ch06-07/Game6.cs
@@ -63,62 +63,37 @@ namespace XNABook_4
 
       TileMap.Initialize(spriteSheet);
 
+      Player.Initialize(
+    spriteSheet,
+      new Rectangle(0, 64, 32, 32),
+      6,
+      new Rectangle(0, 96, 32, 32),
+      1,
+      new Vector2(300, 300)
+    );
+
     }
 
     protected override void Update(GameTime gameTime)
     {
-      if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-        Exit();
+      // Allows the game to exit
+      if (GamePad.GetState(PlayerIndex.One).Buttons.Back ==
+          ButtonState.Pressed)
+        this.Exit();
 
-      // Temporary Demo Code Begin
-      Vector2 spriteMove = Vector2.Zero;
-      Vector2 cameraMove = Vector2.Zero;
-
-      if (Keyboard.GetState().IsKeyDown(Keys.A))
-        spriteMove.X = -1;
-
-      if (Keyboard.GetState().IsKeyDown(Keys.D))
-        spriteMove.X = 1;
-
-      if (Keyboard.GetState().IsKeyDown(Keys.W))
-        spriteMove.Y = -1;
-
-      if (Keyboard.GetState().IsKeyDown(Keys.S))
-        spriteMove.Y = 1;
-
-      if (Keyboard.GetState().IsKeyDown(Keys.Left))
-        cameraMove.X = -1;
-
-      if (Keyboard.GetState().IsKeyDown(Keys.Right))
-        cameraMove.X = 1;
-
-      if (Keyboard.GetState().IsKeyDown(Keys.Up))
-        cameraMove.Y = -1;
-
-      if (Keyboard.GetState().IsKeyDown(Keys.Down))
-        cameraMove.Y = 1;
-
-      Camera.Move(cameraMove);
-      tempSprite.Velocity = spriteMove * 60;
-
-      tempSprite.Update(gameTime);
-      tempSprite2.Update(gameTime);
-      // Temporary Demo Code End
+      Player.Update(gameTime);
 
       base.Update(gameTime);
     }
 
+
     protected override void Draw(GameTime gameTime)
     {
       GraphicsDevice.Clear(Color.CornflowerBlue);
-
-      // Temporary Demo Code Begin
       spriteBatch.Begin();
       TileMap.Draw(spriteBatch);
-      tempSprite.Draw(spriteBatch);
-      tempSprite2.Draw(spriteBatch);
+      Player.Draw(spriteBatch);
       spriteBatch.End();
-      // Temporary Demo Code End
 
       base.Draw(gameTime);
     }
