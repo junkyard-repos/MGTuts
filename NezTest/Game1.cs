85cdb6a0385575edd98b9cf2b7dbde8a2959c9f1
David Long <davdaarn@users.noreply.github.com>
2021-03-20 13:33:54 -0600

    created test project I'll probably never use

--- NezTest/Game1.cs
+++ NezTest/Game1.cs
@@ -1,19 +1,14 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Nez;
 
 namespace NezTest
 {
-  public class Game1 : Game
+  public class Game1 : Core
   {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
-
-    public Game1()
+    public Game1() : base()
     {
-      _graphics = new GraphicsDeviceManager(this);
-      Content.RootDirectory = "Content";
-      IsMouseVisible = true;
     }
 
     protected override void Initialize()
@@ -21,32 +16,12 @@ namespace NezTest
       // TODO: Add your initialization logic here
 
       base.Initialize();
-    }
 
-    protected override void LoadContent()
-    {
-      _spriteBatch = new SpriteBatch(GraphicsDevice);
+      var scene = new Scene();
 
-      // TODO: use this.Content to load your game content here
-    }
+      scene.
 
-    protected override void Update(GameTime gameTime)
-    {
-      if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-        Exit();
-
-      // TODO: Add your update logic here
-
-      base.Update(gameTime);
-    }
-
-    protected override void Draw(GameTime gameTime)
-    {
-      GraphicsDevice.Clear(Color.CornflowerBlue);
-
-      // TODO: Add your drawing code here
-
-      base.Draw(gameTime);
+      Core.Scene = scene;
     }
   }
 }
